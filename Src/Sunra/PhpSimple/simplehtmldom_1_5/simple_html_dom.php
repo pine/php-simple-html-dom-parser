1280ea799a24fb341d23f7985b3054bcbcca942a
Pine Mizune <pinemz@gmail.com>
2016-09-07 20:29:55 +0900

    fix(simple_html_dom.php) fix default max file size

--- Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
+++ Src/Sunra/PhpSimple/simplehtmldom_1_5/simple_html_dom.php
@@ -67,7 +67,7 @@ define('DEFAULT_BR_TEXT', "\r\n");
 define('DEFAULT_SPAN_TEXT', " ");
 if (!defined('MAX_FILE_SIZE'))
 {
-    define('MAX_FILE_SIZE', 600000);
+    define('MAX_FILE_SIZE', 4800000);
 }
 // helper functions
 // -----------------------------------------------------------------------------
